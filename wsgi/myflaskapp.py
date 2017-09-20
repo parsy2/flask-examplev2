c90d53552531fb9457783f88430db0f5cc2cc5b7
parsy2 <p4parush@gmail.com>
2017-09-20 12:39:03 +0530

    Testing GIT

---
+++ wsgi/myflaskapp.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask import render_template
+from wsgi import wsgi
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+if __name__ == "__main__":
+    app.run()
